d171bc6672fc2a78b60d93cd4cdbe0b1009122d3
KKusumi <kusumi_katsumi@andfactory.co.jp>
2021-03-14 23:29:23 +0900

    TableView実装

--- TableViewSample/ViewController.swift
+++ TableViewSample/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let todoList = ["AAA", "BBB", "CCC"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    // セルの個数を指定するデリゲートメソッド
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count
+    }
+    
+    // セルに値を設定するデータソースメソッド
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // idとindexでセルを取得
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        // セルに表示する値を設定する
+        cell.textLabel!.text = todoList[indexPath.row]
+        return cell
+    }
 }
-
